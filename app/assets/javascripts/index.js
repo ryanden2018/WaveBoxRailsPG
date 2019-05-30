73e699de6c9ca9b50b28df8b13eeda08e6f0fd10
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-30 11:48:30 -0500

    scrolling

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(e){
   document.querySelector("#wavebox").append(wb.render());
 
   document.addEventListener("mousemove",
-    function(e) {wb.cursorX=e.x-wb.div.offsetLeft;wb.cursorY=e.y-wb.div.offsetTop-document.body.scrollTop;});
+    function(e) {wb.cursorX=e.x-wb.div.offsetLeft;wb.cursorY=e.y-wb.div.offsetTop+(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);});
   
   setInterval( function() {
     for(var i=0; i<100; i++) {
