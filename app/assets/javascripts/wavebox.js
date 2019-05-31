e05c2757b88f8eea3a5dc0a413cdba993b8f3f8d
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-31 00:16:02 -0500

    rewrite

--- app/assets/javascripts/wavebox.js
+++ app/assets/javascripts/wavebox.js
@@ -79,61 +79,64 @@ function WaveBox(width,dt) {
 
   // step forward the physical model
   this.stepForward = function() {
-   
+
     var Mat = [];
     var Mat2 = [];
+    var BMat = [];
+    var BMat2 = [];
+
     for(var i=0; i<this.width; i++) {
-
       for(var j=0; j<this.width; j++) {
-        if((i===0) || (i===this.width-1) ||(j===0) || (j===this.width-1)) {Mat.push(0.0);Mat2.push(0.0);}
-         else {
-        var idx = this.idx(i,j);
-        Mat.push(
-          this.Dimage[idx] +
-           this.dt*(Math.sin(this.c/10))*Math.exp((-10)*(Math.pow(this.pxSz()*i-this.cursorY,2)/(50*50)+Math.pow(this.pxSz()*j-this.cursorX,2)/(50*50))) -
+        if((i===0)||(j===0)||(i===this.width-1)||(j===this.width-1)) {
+          Mat.push(0.0);
+          Mat2.push(0.0);
+          BMat.push(0.0);
+          BMat2.push(0.0);
+        } else {
+          var idx = this.idx(i,j);
+          Mat.push(this.image[idx]);
+          Mat2.push(
+            this.Dimage[idx]
+            +this.dt*(Math.sin(this.c/10))*Math.exp((-10)*(Math.pow(this.pxSz()*i-this.cursorY,2)/(50*50)+Math.pow(this.pxSz()*j-this.cursorX,2)/(50*50))) -
            this.dt*0.025*this.Dimage[idx]
-        );
-        Mat2.push(Mat[idx]);
-
-        this.image[idx] =this.image[idx]+this.dt*this.Dimage[idx];
-        this.Dimage[idx] = Mat[idx];
+          );
+          BMat.push(Mat[idx]);
+          BMat2.push(Mat2[idx]);
+          this.Dimage[idx] = Mat2[idx];
         }
       }
-     
     }
 
-  for(var k = 0; k < 5; k++) {
-    for(var i=1; i<this.width-1; i++) {
-      for(var j=1; j<this.width-1; j++) {
-        var idx = this.idx(i,j);
-        var idxU = this.idx(i-1,j);
-        var idxD = this.idx(i+1,j);
-        var idxL = this.idx(i,j-1);
-        var idxR = this.idx(i,j+1);
-        Mat[idx] = 10*(Mat2[idxU]+Mat2[idxD]+
-          Mat2[idxL]+Mat2[idxR]-4*Mat2[idx]) * this.dt;
-        this.Dimage[idx] += Mat[idx]; 
-  
+    for(var k=0; k<5; k++) {
+      for(var i=1; i<this.width-1; i++) {
+        for(var j=1; j<this.width-1; j++) {
+          var idx = this.idx(i,j);
+          var idxU = this.idx(i-1,j);
+          var idxD = this.idx(i+1,j);
+          var idxL = this.idx(i,j-1);
+          var idxR = this.idx(i,j+1);
+          BMat[idx] = Mat2[idx]*this.dt;
+          BMat2[idx] = this.dt*(Mat[idxU]+Mat[idxD]+Mat[idxL]+Mat[idxR]-4*Mat[idx]);
+          this.image[idx] += BMat[idx];
+          this.Dimage[idx] += BMat2[idx];
+        }
+      }
+
+      for(var i=1; i<this.width-1; i++) {
+        for(var j=1; j<this.width-1; j++) {
+          var idx = this.idx(i,j);
+          var idxU = this.idx(i-1,j);
+          var idxD = this.idx(i+1,j);
+          var idxL = this.idx(i,j-1);
+          var idxR = this.idx(i,j+1);
+          Mat[idx] = BMat2[idx]*this.dt;
+          Mat2[idx] = this.dt*(BMat[idxU]+BMat[idxD]+BMat[idxL]+BMat[idxR]-4*BMat[idx]);
+          this.image[idx] += Mat[idx];
+          this.Dimage[idx] += Mat2[idx];
+        }
       }
     }
 
-    for(var i=1; i<this.width-1; i++) {
-      for(var j=1; j<this.width-1; j++) {
-        var idx = this.idx(i,j);
-        var idxU = this.idx(i-1,j);
-        var idxD = this.idx(i+1,j);
-        var idxL = this.idx(i,j-1);
-        var idxR = this.idx(i,j+1);
-        Mat2[idx] = 10*(Mat[idxU]+Mat[idxD]+
-          Mat[idxL]+Mat[idxR]-4*Mat[idx]) * this.dt;
-        this.Dimage[idx] += Mat2[idx]; 
-  
-      }
-    }
 
-    }
-
- }
-
-
+  }
 }
