00477be14020a0675bd30a1c7166d744c2d03302
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-31 22:06:24 -0500

    normalize colors

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,9 +1,9 @@
-//document.addEventListener("DOMContentLoaded", function(e){
+
 window.onload = function() {
-  //document.querySelector("#wavebox").append(wb.render());
 
 
   var canvas = document.querySelector("#wb");
+  var innerbox = document.querySelector("#innerbox");
   var context = canvas.getContext("2d");
   var width = canvas.width;
   var height = canvas.height;
@@ -16,7 +16,7 @@ window.onload = function() {
     for(var i=0; i<width; i++) {
       for(var j=0; j<width; j++) {
         var idx0 = (i*width+j)*4;
-        var val = Math.floor(Math.max(0.0,Math.min(1.0,wb.image[i*width+j]))*255);
+        var val = Math.floor(Math.max(0.0,Math.min(1.0,0.5+0.05*(wb.image[i*width+j]-0.5)))*255);
         imgdata.data[idx0] = val;
         imgdata.data[idx0+1] = val;
         imgdata.data[idx0+2] = val;
@@ -26,28 +26,16 @@ window.onload = function() {
   }
 
   document.addEventListener("mousemove",
-    function(e) {wb.cursorX=e.x-10-canvas.offsetLeft;wb.cursorY=e.y-160+(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);});
+    function(e) {wb.cursorX=e.x-10-innerbox.offsetLeft;wb.cursorY=e.y-160+(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);});
 
    document.addEventListener("touchmove",
-    function(e) {wb.cursorX=e.changedTouches[0].screenX-10-canvas.offsetLeft;wb.cursorY=e.changedTouches[0].screenY-160+(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);});
+    function(e) {wb.cursorX=e.changedTouches[0].screenX-10-innerbox.offsetLeft;wb.cursorY=e.changedTouches[0].screenY-160+(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);});
 
     document.addEventListener("touchstart",
-    function(e) {wb.cursorX=e.changedTouches[0].screenX-10-canvas.offsetLeft;wb.cursorY=e.changedTouches[0].screenY-160+(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);});
+    function(e) {wb.cursorX=e.changedTouches[0].screenX-10-innerbox.offsetLeft;wb.cursorY=e.changedTouches[0].screenY-160+(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);});
 
     document.addEventListener("touchend",
-    function(e) {wb.cursorX=e.changedTouches[0].screenX-10-canvas.offsetLeft;wb.cursorY=e.changedTouches[0].screenY-160+(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);});
-
-
-
-  
-  /*setInterval( function() {
-      wb.stepForward();
-      wb.c += 1;
-  }, 60);
-
-  setInterval( function() {
-    wb.update();
-  },50);*/
+    function(e) {wb.cursorX=e.changedTouches[0].screenX-10-innerbox.offsetLeft;wb.cursorY=e.changedTouches[0].screenY-160+(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);});
 
   function main(tf) {
     window.requestAnimationFrame(main);
